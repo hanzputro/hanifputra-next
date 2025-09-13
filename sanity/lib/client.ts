e5c9c074e596ed6446187501c81911c7155fb153
hanzputro <hanzputro@gmail.com>
2025-09-13 21:51:06 +0700

    fix sanity client

--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -3,8 +3,8 @@ import { createClient } from "next-sanity";
 import { apiVersion, dataset, projectId } from "@/sanity/env";
 
 export const client = createClient({
-  projectId,
-  dataset,
+  projectId: process.env.NEXT_PUBLIC_SANITY_STUDIO_PROJECT_ID || process.env.SANITY_STUDIO_PROJECT_ID,
+  dataset: process.env.NEXT_PUBLIC_SANITY_STUDIO_DATASET || process.env.SANITY_STUDIO_DATASET,
   apiVersion,
   useCdn: true, // Set to false if statically generating pages, using ISR or tag-based revalidation
 });
